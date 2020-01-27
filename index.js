129c94fd7a6526a102d1441d55b1e26b5d5737e9
Diego <egovaldel@gmail.com>
2020-01-27 23:37:37 +0100

    1

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const mongoose = require ('mongoose')
 const express = require ('express')
 const app = express()
-const port = process.env.port || 3003 
+const port = process.env.port || 3004 
 const person = require('./routes/person')
 app.use(express.json())
 app.use('/api/persons/', person)
 // Asignación de puerto por el que nos dan o por defecto el 3003
 app.listen(port, () => console.log('Listening on port: ' + port))
 // Conexión a la base de DOMSettableTokenList, devuelve una promesa por lo que puedo usar then/catch
-mongoose.connect('mongodb://localhost/figuraciondb', {useNewUrlParser:true, useUnifiedTopology: true })
+mongoose
+    .connect('mongodb://localhost/figuraciondb', {useNewUrlParser:true, useUnifiedTopology: true })
     .then( () =>    console.log('Connected to MongoDB'))
     .catch(error => console.log('Error conecting to MongoDB'))
--- routes/person.js
+++ routes/person.js
@@ -1,12 +1,13 @@
 const mongosee = require('mongoose')
+mongosee.set('useFindAndModify', false);
 const express = require('express')
 const Person = require('../models/person') //Importo el modulo de personas
 const router = express.Router() //Importo el modulo para enrutar las paginas
 const { check, validationResult } = require('express-validator')
 
 router.post('/', [
-    // check('email').isEmail()
-],
+    check('email').isEmail()
+    ],
     async(req,res) => {
         const errors = validationResult(req)
         if(!errors.isEmpty()) return res.status(422).json({errors: errors.array()})
@@ -20,4 +21,34 @@ router.post('/', [
     }
 
 )
+
+router.get('/', async(req,res) => {
+    const personAll = await  Person.find()
+    res.send(personAll)
+})
+
+router.get('/:id', async(req,res) => {
+    try {
+        const person = await Person.findById(req.params.id)
+        res.send(person)
+    } catch (e) {
+        res.status(404).send({error: 'Persona no encontrada: ' + e })
+    }
+})
+
+router.patch('/:id', async(req, res) => {
+    try {
+        const filter = { _id: req.params.id}
+        const update = { name: req.body.name, fisrtSurname: req.body.fisrtSurname , lastSurname: req.body.lastSurname}
+        let person = await Person.findOneAndUpdate(filter, update)
+        res.send('Nuevo: ' + person)
+    }catch (e) {
+        res.status(404).send({error: 'Error al actualizar el registro: ' + e })
+    }
+})
+
+router.delete ('/:id', async(req,res) => {
+
+})
+
 module.exports = router
